c12e71b127c62e4bf4abbe9a49cb4f0d27b7b356
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-22 00:36:41 +0700

    feat: splashscreen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 import store, { persistor } from './store';
 import Routes from './Routes';
 import { Colors } from './Theme';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <>
       <Provider store={store}>
