c0c867b1c3faedc539fdf74ad2394c21f05a5e0a
Firmansyah Yanuar <fyfirman@gmail.com>
2020-09-02 10:12:02 +0700

    feat: move sign out button to bottom and add header section

--- src/Routes/MenuDrawer/CustomDrawer/CustomDrawer.js
+++ src/Routes/MenuDrawer/CustomDrawer/CustomDrawer.js
@@ -1,26 +1,49 @@
 import React from 'react';
+import { View } from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem
 } from '@react-navigation/drawer';
+import { Avatar, Title, Caption, Paragraph } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
+import * as styles from './styles';
+import { Colors } from '../../../Theme';
 
 const CustomDrawer = (props) => {
-  const { ...rest } = props;
-
-  // const getIconName = routeName;
-
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Keluar"
-        onPress={() => {}}
-        icon={({ focused, color, size }) => (
-          <Icon color={color} size={size} name="log-out-outline" />
-        )}
+    <DrawerContentScrollView {...props} contentContainerStyle={styles.root}>
+      <View style={styles.userInfoSection}>
+        <Avatar.Image
+          source={{
+            uri:
+              'https://pbs.twimg.com/profile_images/952545910990495744/b59hSXUd_400x400.jpg'
+          }}
+          size={50}
+        />
+        <Title style={styles.title}>Dawid Urbaniak</Title>
+        <Caption style={styles.caption}>@trensik</Caption>
+      </View>
+      <DrawerItemList
+        {...props}
+        style={styles.menuItems}
+        labelStyle={styles.labelStyle}
+        activeTintColor={Colors.java}
+        inactiveTintColor={Colors.textPrimary}
       />
+      <View style={styles.footer}>
+        <DrawerItem
+          label="Keluar"
+          onPress={() => {}}
+          style={styles.signOutItem}
+          labelStyle={styles.labelStyle}
+          activeTintColor={Colors.java}
+          inactiveTintColor={Colors.textPrimary}
+          icon={({ color, size }) => (
+            <Icon color={color} size={size} name="log-out-outline" />
+          )}
+        />
+      </View>
     </DrawerContentScrollView>
   );
 };
---
+++ src/Routes/MenuDrawer/CustomDrawer/styles.js
@@ -0,0 +1,21 @@
+import { FontFamily } from '../../../Theme';
+
+export const root = {
+  flex: 1
+};
+
+export const menuItems = {
+  flex: 1,
+  justifyContent: 'space-between'
+};
+
+export const labelStyle = {
+  fontFamily: FontFamily.OpenSans.semiBold
+};
+
+export const footer = {
+  flex: 1,
+  flexDirection: 'column-reverse'
+};
+
+export const signOutItem = {};
