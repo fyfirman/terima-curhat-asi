55c5ee8ed465d23d97979b8061c768233dba030e
Firmansyah Yanuar <fyfirman@gmail.com>
2020-09-03 23:21:05 +0700

    refactor: redux persist

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
-
+import { persistReducer } from 'redux-persist';
+import { persistConfig } from '../Config';
 import SessionReducer from './SessionReducer';
 import UserReducer from './UserReducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
-  user: UserReducer
+  user: persistReducer(persistConfig, UserReducer)
 });
 
 export default rootReducer;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
-import { persistConfig } from './Config';
 import LoggerMiddleware from './Middlewares/LoggerMiddleware';
 import monitorReducer from './Enhancers/monitorReducer';
 import rootReducer from './Reducers';
@@ -9,11 +8,7 @@ import rootReducer from './Reducers';
 const middleware = applyMiddleware(LoggerMiddleware, thunk);
 const enhancer = compose(middleware, monitorReducer);
 
-const store = createStore(
-  persistReducer(persistConfig, rootReducer),
-  undefined,
-  enhancer
-);
+const store = createStore(rootReducer, undefined, enhancer);
 
 export const persistor = persistStore(store);
 export default store;
