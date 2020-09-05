c93a1c664a43797612475aec88d29547634a4015
Firmansyah Yanuar <fyfirman@gmail.com>
2020-09-05 12:47:54 +0700

    feat: implement prompt

--- src/Routes/MenuDrawer/CustomDrawer/CustomDrawer.js
+++ src/Routes/MenuDrawer/CustomDrawer/CustomDrawer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, TouchableOpacity } from 'react-native';
 import {
@@ -8,7 +8,7 @@ import {
 } from '@react-navigation/drawer';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Avatar } from 'react-native-paper';
+import { Avatar, Button, Paragraph, Dialog, Portal } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { SessionAction, UserAction } from '../../../Redux/Actions';
 import * as styles from './styles';
@@ -24,6 +24,8 @@ const defaultProps = {};
 
 const CustomDrawer = (props) => {
   const { flushUser, user, navigation } = props;
+
+  const [promptVisibility, setPromptVisibility] = useState(false);
 
   const logout = () => {
     flushUser();
@@ -55,7 +57,7 @@ const CustomDrawer = (props) => {
       <View style={styles.footer}>
         <DrawerItem
           label="Keluar"
-          onPress={logout}
+          onPress={() => setPromptVisibility(true)}
           style={styles.signOutItem}
           labelStyle={styles.labelStyle}
           inactiveTintColor={Colors.redAlert}
@@ -64,6 +66,31 @@ const CustomDrawer = (props) => {
           )}
         />
       </View>
+      <Portal>
+        <Dialog
+          visible={promptVisibility}
+          onDismiss={() => setPromptVisibility(false)}
+        >
+          <Dialog.Title>Apakah kamu yakin akan keluar?</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>
+              Dengan menekan tombol yakin, akun anda akan tidak tertaut dengan
+              aplikasi. Anda bisa masuk kembali dengan akun yang anda miliki
+            </Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={() => setPromptVisibility(false)}
+              color={Colors.cranberry}
+            >
+              Kembali
+            </Button>
+            <Button onPress={logout} color={Colors.cranberry}>
+              Yakin
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </DrawerContentScrollView>
   );
 };
