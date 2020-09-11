6582d032efad71c2f8920ab5d6f81b4165bba300
Firmansyah Yanuar <fyfirman@gmail.com>
2020-09-11 20:29:40 +0700

    feat: add random icon

--- src/Screens/Consultation/New/Components/NewConsultCard/NewConsultCard.js
+++ src/Screens/Consultation/New/Components/NewConsultCard/NewConsultCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
+import { Avatar } from 'react-native-paper';
 import * as styles from './styles';
 import { Button } from './Components';
 
@@ -17,12 +18,20 @@ const NewConsultCard = (props) => {
 
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.ava}
-        source={require('../../../../../assets/images/logo.png')}
-      />
+      <TouchableOpacity onPress={() => {}} style={styles.avaContainer}>
+        <Avatar.Image
+          style={styles.ava}
+          source={{
+            uri: `https://api.adorable.io/avatars/${Math.random() * 5000}`
+          }}
+          size={60}
+        />
+      </TouchableOpacity>
       <View style={styles.infoContainer}>
         <Text style={styles.name}>{name}</Text>
+        <Text style={styles.info}>
+          Tekan pada foto profil untuk melihat profil
+        </Text>
         <View style={styles.buttonContainer}>
           <Button title="Tolak" onPress={() => {}} secondary />
           <Button title="Terima" onPress={() => {}} />
--- src/Screens/Consultation/New/Components/NewConsultCard/styles.js
+++ src/Screens/Consultation/New/Components/NewConsultCard/styles.js
@@ -4,14 +4,18 @@ const AVA_SIZE = 60;
 
 export const container = {
   flexDirection: 'row',
-  marginTop: 8
+  marginTop: 8,
+  borderBottomWidth: 1,
+  borderBottomColor: Colors.borderBottomCard
+};
+
+export const avaContainer = {
+  justifyContent: 'center'
 };
 
 export const ava = {
   width: AVA_SIZE,
-  height: AVA_SIZE,
-  alignSelf: 'center',
-  paddingVertical: 8
+  height: AVA_SIZE
 };
 
 export const name = {
@@ -19,12 +23,16 @@ export const name = {
   fontSize: 16
 };
 
+export const info = {
+  fontFamily: FontFamily.OpenSans.regular,
+  fontSize: 14,
+  color: Colors.textPrimary
+};
+
 export const infoContainer = {
   flex: 1,
   paddingVertical: 8,
-  paddingHorizontal: 8,
-  borderBottomWidth: 1,
-  borderBottomColor: Colors.borderBottomCard
+  paddingHorizontal: 8
 };
 
 export const buttonContainer = {
