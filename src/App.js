9e068e81b5e3372f2ba73efb6ae12f6d5e6a47e2
Firmansyah Yanuar <fyfirman@gmail.com>
2020-09-18 19:10:54 +0700

    feat: setup pusher

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { PersistGate } from 'redux-persist/integration/react';
 import SplashScreen from 'react-native-splash-screen';
+import Pusher from 'pusher-js/react-native';
+import Echo from 'laravel-echo';
 import { LoadingScreen } from './Components';
 import store, { persistor } from './Redux/store';
 import Routes from './Routes';
@@ -12,6 +14,25 @@ import { Colors } from './Theme';
 
 const App = () => {
   useEffect(() => {
+    Pusher.logToConsole = true;
+
+    const PusherClient = new Pusher('8ce4048a46a0c08c9cd8', {
+      cluster: 'ap1',
+      wsHost: '192.168.1.11',
+      wsPort: '8000',
+      enabledTransports: ['ws'],
+      forceTLS: false
+    });
+
+    const echo = new Echo({
+      broadcaster: 'pusher',
+      client: PusherClient
+    });
+
+    echo.private(`consultations.${2}`).notification((data) => {
+      console.log(data);
+    });
+
     SplashScreen.hide();
   });
 
