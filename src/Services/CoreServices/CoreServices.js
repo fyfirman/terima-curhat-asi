b17970ad14df34431293794edff0fa408c6fcf45
Firmansyah Yanuar <fyfirman@gmail.com>
2020-09-23 23:05:13 +0700

    fix: revert chat message order

--- src/Services/CoreServices/CoreServices.js
+++ src/Services/CoreServices/CoreServices.js
@@ -7,7 +7,10 @@ const getProfile = () => get('api/profile', Config.withToken());
 const getConsultations = (config) =>
   get('api/consultations', Config.withToken(config));
 const getConsultationPost = (id) =>
-  get(`api/consultations/${id}/consultation_posts`, Config.withToken());
+  get(
+    `api/consultations/${id}/consultation_posts?sortBy=desc`,
+    Config.withToken()
+  );
 
 // Post
 const postGenerateToken = (data) => post('auth/token', Config.withOauth(data));
