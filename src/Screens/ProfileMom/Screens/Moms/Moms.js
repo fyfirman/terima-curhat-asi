f89a68390b0d53c8bab13514e4388205e397520c
Firmansyah Yanuar <fyfirman@gmail.com>
2020-10-15 14:45:27 +0700

    feat: add more attribute

--- src/Screens/ProfileMom/Screens/Moms/Moms.js
+++ src/Screens/ProfileMom/Screens/Moms/Moms.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ScrollView } from 'react-native';
 import { InfoItem } from '../../Components';
+import { StringHelper } from '../../../../Helper';
 
 const propTypes = {
   route: PropTypes.objectOf(PropTypes.any).isRequired
@@ -17,8 +18,12 @@ const Moms = (props) => {
     <ScrollView>
       <InfoItem label="Nama" info={user.profile.name} />
       {/* TODO: format date of birth */}
+      <InfoItem label="Umur" info={user.profile.age.toString()} />
       <InfoItem label="Tanggal Lahir" info={user.profile.dob} />
-      <InfoItem label="Agama" info={user.profile.religion} />
+      <InfoItem
+        label="Agama"
+        info={StringHelper.toTitleCase(user.profile.religion)}
+      />
       <InfoItem label="Golongan Darah" info={user.profile.blood_type} />
       <InfoItem
         label="Status Pernikahan"
@@ -27,6 +32,11 @@ const Moms = (props) => {
       <InfoItem label="Pendidikan" info={user.profile.education_text} />
       <InfoItem label="Pekerjaan/Profesi" info={user.profile.occupation.name} />
       <InfoItem label="Alamat" info={user.profile.full_address} />
+      <InfoItem label="Tinggal Dengan" info={user.profile.housemate_text} />
+      <InfoItem
+        label="Kepemilikan Rumah"
+        info={user.profile.house_ownership_text}
+      />
     </ScrollView>
   );
 };
