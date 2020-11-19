3851cf233f7f01f27a5f2f59151fc0af1ff9770f
Firmansyah Yanuar <fyfirman@gmail.com>
2020-11-19 22:48:00 +0700

    feat: setup pusher beams

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  assets: ['./src/assets/fonts/']
+  assets: ['./src/assets/fonts/'],
+  dependencies: {
+    'react-native-pusher-push-notifications': {
+      platforms: {
+        android: null // this skips autolink for android
+      }
+    }
+  }
 };
