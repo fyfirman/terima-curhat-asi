74e0c061dda16c53bf164174621a367360361571
Firmansyah Yanuar <fyfirman@gmail.com>
2021-02-08 19:29:44 +0700

    fix: auth pusher

--- src/Services/ChatServices/ChatServices.js
+++ src/Services/ChatServices/ChatServices.js
@@ -2,24 +2,26 @@ import Pusher from 'pusher-js/react-native';
 import {
   PUSHER_APP_CLUSTER,
   PUSHER_APP_KEY,
-  PUSHER_AUTH_ENDPOINT
+  PUSHER_AUTH_ENDPOINT,
+  PUSHER_APP_ID,
+  PUSHER_LOG
 } from 'react-native-dotenv';
 
 const create = (session) => {
   const options = {
+    appId: PUSHER_APP_ID,
     cluster: PUSHER_APP_CLUSTER,
-    forceTLS: true,
     authEndpoint: PUSHER_AUTH_ENDPOINT,
+    encrypted: true,
     auth: {
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
         Authorization: `${session.tokenType} ${session.accessToken}`
       }
     }
   };
 
-  Pusher.logToConsole = false;
+  Pusher.logToConsole = PUSHER_LOG;
   const PusherClient = new Pusher(PUSHER_APP_KEY, options);
 
   return PusherClient;
