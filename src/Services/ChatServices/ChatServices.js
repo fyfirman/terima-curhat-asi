e71d3196d68f6f6a97f4caf74065823330382967
Firmansyah Yanuar <fyfirman@gmail.com>
2021-02-10 05:40:06 +0700

    feat: env

--- src/Services/ChatServices/ChatServices.js
+++ src/Services/ChatServices/ChatServices.js
@@ -1,5 +1,6 @@
 import Pusher from 'pusher-js/react-native';
 import {
+  BASE_URL,
   PUSHER_APP_CLUSTER,
   PUSHER_APP_KEY,
   PUSHER_AUTH_ENDPOINT,
@@ -11,7 +12,7 @@ const create = (session) => {
   const options = {
     appId: PUSHER_APP_ID,
     cluster: PUSHER_APP_CLUSTER,
-    authEndpoint: PUSHER_AUTH_ENDPOINT,
+    authEndpoint: BASE_URL + PUSHER_AUTH_ENDPOINT,
     encrypted: true,
     auth: {
       headers: {
