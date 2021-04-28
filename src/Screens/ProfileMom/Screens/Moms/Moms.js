d8dc8f8b0a50f4597ac577763b4acaa6761a94b6
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-28 21:52:50 +0700

    fix: nullish profile moms

--- src/Screens/ProfileMom/Screens/Moms/Moms.js
+++ src/Screens/ProfileMom/Screens/Moms/Moms.js
@@ -16,32 +16,44 @@ const Moms = (props) => {
 
   return (
     <ScrollView>
-      <ProfileInfoItem label="Nama" info={user.profile?.name} />
+      <ProfileInfoItem label="Nama" info={user.profile?.name ?? '-'} />
       {/* TODO: format date of birth */}
-      <ProfileInfoItem label="Umur" info={user.profile?.age.toString()} />
-      <ProfileInfoItem label="Tanggal Lahir" info={user.profile?.dob} />
+      <ProfileInfoItem
+        label="Umur"
+        info={user.profile?.age.toString() ?? '-'}
+      />
+      <ProfileInfoItem label="Tanggal Lahir" info={user.profile?.dob ?? '-'} />
       <ProfileInfoItem
         label="Agama"
         info={StringHelper.toTitleCase(user.profile?.religion || '')}
       />
-      <ProfileInfoItem label="Golongan Darah" info={user.profile?.blood_type} />
+      <ProfileInfoItem
+        label="Golongan Darah"
+        info={user.profile?.blood_type ?? '-'}
+      />
       <ProfileInfoItem
         label="Status Pernikahan"
         info={user.profile?.marital_status_text}
       />
-      <ProfileInfoItem label="Pendidikan" info={user.profile?.education_text} />
+      <ProfileInfoItem
+        label="Pendidikan"
+        info={user.profile?.education_text ?? '-'}
+      />
       <ProfileInfoItem
         label="Pekerjaan/Profesi"
-        info={user.profile?.occupation.name}
+        info={user.profile?.occupation?.name ?? '-'}
       />
-      <ProfileInfoItem label="Alamat" info={user.profile?.full_address} />
+      <ProfileInfoItem
+        label="Alamat"
+        info={user.profile?.full_address ?? '-'}
+      />
       <ProfileInfoItem
         label="Tinggal Dengan"
-        info={user.profile?.housemate_text}
+        info={user.profile?.housemate_text ?? '-'}
       />
       <ProfileInfoItem
         label="Kepemilikan Rumah"
-        info={user.profile?.house_ownership_text}
+        info={user.profile?.house_ownership_text ?? '-'}
       />
     </ScrollView>
   );
